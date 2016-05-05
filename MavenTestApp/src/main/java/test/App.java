e736f54f3e66587ddb0a9c59b40aeaf9351e8526
rafal-izbinski <rafal.izbinski@gmail.com>
2016-05-05 19:25:28 +0200

    Projekt domowy
    Zadanie domowe

---
+++ MavenTestApp/src/main/java/test/App.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	StringUtils.capitalize("");
+        System.out.println( "Hello World!" );
+    }
+}
